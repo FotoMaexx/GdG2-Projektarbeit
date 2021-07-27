7ef4201e4a1e428e30ac0e4057e80c6090e97488
MrMxDzn <info@hauser-media.com>
2021-07-27 14:53:34 +0200

    Map hinzugefügt

---
+++ scroll_demo-gh-pages/js/map.js
@@ -0,0 +1,13 @@
+var freiburgmap = L.map('freiburg').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'sk.eyJ1IjoibXJteGR6biIsImEiOiJja3JtMTJqd2YwaWwwMnFudnM0d2FnNnhoIn0.OoAiV0D5lXGH-beUJKX5uA'
+}).addTo(freiburgmap);
+
+var mapShower = document.getElementById('freiburg');
+mapShower.style.position = null;
